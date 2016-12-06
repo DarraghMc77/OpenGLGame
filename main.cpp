9fe920d90d4ca88c20beb0b2ae0e640b2e2b9921
Darragh Mccaffrey <mccaffrd@ITSERV.SCSS.TCD.IE>
2016-12-06 21:17:29 +0000

    Added loading in of multiple meshes, player class, ball class and throwing animation

--- main.cpp
+++ main.cpp
@@ -27,16 +27,23 @@
   ----------------------------------------------------------------------------*/
 // this mesh is a dae file format but you should be able to use any other format too, obj is typically what is used
 // put the mesh in your project directory, or provide a filepath for it here
-#define MESH_NAME "../neweststadium.obj"
-#define MESH_NAME2 "../footballsmall.obj"
+#define STADIUM_NAME "../neweststadium.obj"
+#define PLAYER_NAME "../luiginoarm.obj"
+#define BALL_NAME "../footballsmall.obj"
+#define ENEMY_NAME "../patrick.obj"
+#define PLAYER_ARM "../luigiarmonaxis.obj"
 /*----------------------------------------------------------------------------
   ----------------------------------------------------------------------------*/
 
 #define PI 3.14159265359
 
-std::vector<float> g_vp, g_vn, g_vt;
-int g_point_count = 0;
 
+//change if more meshes loaded
+int numberOfMeshes = 5;
+
+int g_point_count[5];
+std::vector<float> g_vp[5], g_vn[5], g_vt[5];
+unsigned int vaos[5];
 
 // Macro for indexing vertex buffer
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
@@ -51,15 +58,6 @@ int height = 600;
 
 GLuint loc1, loc2, loc3;
 GLfloat rotate_y = 0.0f;
-
-unsigned int vao;
-unsigned int vao2;
-
-//second mesh
-std::vector<float> g_vp2, g_vn2, g_vt2;
-int g_point_count2 = 0;
-//GLuint loc4, loc5, loc6;
-
 
 //camera controls
 glm::vec3 cameraPos = glm::vec3(0.0f, 2.0f, 3.0f);
@@ -90,8 +88,7 @@ bool thirdPerson = true;
 
 bool throwBall = false;
 
-//enemies
-
+bool throwing = false;
 
 class Enemy {
 	float xposition;
@@ -129,6 +126,56 @@ class Enemy {
 		}
 };
 
+class Player {
+	float xposition;
+	float yposition;
+	float zposition;
+public:
+	void setPos(float x, float y, float z) {
+		xposition = x;
+		yposition = y;
+		zposition = z;
+	}
+
+	float getXPos() {
+		return xposition;
+	}
+
+	float getYPos() {
+		return yposition;
+	}
+
+	float getZPos() {
+		return zposition;
+	}
+};
+
+class Ball {
+	float xposition;
+	float yposition;
+	float zposition;
+public:
+	void setPos(float x, float y, float z) {
+		xposition = x;
+		yposition = y;
+		zposition = z;
+	}
+
+	float getXPos() {
+		return xposition;
+	}
+
+	float getYPos() {
+		return yposition;
+	}
+
+	float getZPos() {
+		return zposition;
+	}
+};
+
+
+
 Enemy enemies[5];
 
 #pragma region MESH LOADING
@@ -152,103 +199,55 @@ void calculateCameraPosition() {
 	cameraPos = glm::vec3((xplayer), (yplayer + 0.5f), (zplayer + 1.0));
 }
 
-bool load_mesh (const char* file_name) {
-  const aiScene* scene = aiImportFile (file_name, aiProcess_Triangulate); // TRIANGLES!
-        fprintf (stderr, "ERROR: reading mesh %s\n", file_name);
-  if (!scene) {
-    fprintf (stderr, "ERROR: reading mesh %s\n", file_name);
-    return false;
-  }
-  printf ("  %i animations\n", scene->mNumAnimations);
-  printf ("  %i cameras\n", scene->mNumCameras);
-  printf ("  %i lights\n", scene->mNumLights);
-  printf ("  %i materials\n", scene->mNumMaterials);
-  printf ("  %i meshes\n", scene->mNumMeshes);
-  printf ("  %i textures\n", scene->mNumTextures);
-  g_point_count = 0;
-  
-  for (unsigned int m_i = 0; m_i < scene->mNumMeshes; m_i++) {
-    const aiMesh* mesh = scene->mMeshes[m_i];
-    printf ("    %i vertices in mesh\n", mesh->mNumVertices);
-    g_point_count += mesh->mNumVertices;
-    for (unsigned int v_i = 0; v_i < mesh->mNumVertices; v_i++) {
-      if (mesh->HasPositions ()) {
-        const aiVector3D* vp = &(mesh->mVertices[v_i]);
-        //printf ("      vp %i (%f,%f,%f)\n", v_i, vp->x, vp->y, vp->z);
-        g_vp.push_back (vp->x);
-        g_vp.push_back (vp->y);
-        g_vp.push_back (vp->z);
-      }
-      if (mesh->HasNormals ()) {
-        const aiVector3D* vn = &(mesh->mNormals[v_i]);
-        //printf ("      vn %i (%f,%f,%f)\n", v_i, vn->x, vn->y, vn->z);
-        g_vn.push_back (vn->x);
-        g_vn.push_back (vn->y);
-        g_vn.push_back (vn->z);
-      }
-      if (mesh->HasTextureCoords (0)) {
-        const aiVector3D* vt = &(mesh->mTextureCoords[0][v_i]);
-        //printf ("      vt %i (%f,%f)\n", v_i, vt->x, vt->y);
-        g_vt.push_back (vt->x);
-        g_vt.push_back (vt->y);
-      }
-      if (mesh->HasTangentsAndBitangents ()) {
-        // NB: could store/print tangents here
-      }
-    }
-  }
-  
-  aiReleaseImport (scene);
-  return true;
-}
-
-bool load_mesh2(const char* file_name) {
-	const aiScene* scene = aiImportFile(file_name, aiProcess_Triangulate); // TRIANGLES!
-	fprintf(stderr, "ERROR: reading mesh %s\n", file_name);
-	if (!scene) {
+bool load_mesh(const char* file_name[]) {
+	for (int i = 0; i < numberOfMeshes; i++) {
+		const aiScene* scene = aiImportFile(file_name[i], aiProcess_Triangulate); // TRIANGLES!
 		fprintf(stderr, "ERROR: reading mesh %s\n", file_name);
-		return false;
-	}
-	printf("  %i animations\n", scene->mNumAnimations);
-	printf("  %i cameras\n", scene->mNumCameras);
-	printf("  %i lights\n", scene->mNumLights);
-	printf("  %i materials\n", scene->mNumMaterials);
-	printf("  %i meshes\n", scene->mNumMeshes);
-	printf("  %i textures\n", scene->mNumTextures);
-	g_point_count2 = 0;
+		if (!scene) {
+			fprintf(stderr, "ERROR: reading mesh %s\n", file_name);
+			return false;
+		}
+		printf("  %i animations\n", scene->mNumAnimations);
+		printf("  %i cameras\n", scene->mNumCameras);
+		printf("  %i lights\n", scene->mNumLights);
+		printf("  %i materials\n", scene->mNumMaterials);
+		printf("  %i meshes\n", scene->mNumMeshes);
+		printf("  %i textures\n", scene->mNumTextures);
+		g_point_count[i] = 0;
 
-	for (unsigned int m_i = 0; m_i < scene->mNumMeshes; m_i++) {
-		const aiMesh* mesh = scene->mMeshes[m_i];
-		printf("    %i vertices in mesh\n", mesh->mNumVertices);
-		g_point_count2 += mesh->mNumVertices;
-		for (unsigned int v_i = 0; v_i < mesh->mNumVertices; v_i++) {
-			if (mesh->HasPositions()) {
-				const aiVector3D* vp = &(mesh->mVertices[v_i]);
-				//printf ("      vp %i (%f,%f,%f)\n", v_i, vp->x, vp->y, vp->z);
-				g_vp2.push_back(vp->x);
-				g_vp2.push_back(vp->y);
-				g_vp2.push_back(vp->z);
-			}
-			if (mesh->HasNormals()) {
-				const aiVector3D* vn = &(mesh->mNormals[v_i]);
-				//printf ("      vn %i (%f,%f,%f)\n", v_i, vn->x, vn->y, vn->z);
-				g_vn2.push_back(vn->x);
-				g_vn2.push_back(vn->y);
-				g_vn2.push_back(vn->z);
-			}
-			if (mesh->HasTextureCoords(0)) {
-				const aiVector3D* vt = &(mesh->mTextureCoords[0][v_i]);
-				//printf ("      vt %i (%f,%f)\n", v_i, vt->x, vt->y);
-				g_vt2.push_back(vt->x);
-				g_vt2.push_back(vt->y);
-			}
-			if (mesh->HasTangentsAndBitangents()) {
-				// NB: could store/print tangents here
+		for (unsigned int m_i = 0; m_i < scene->mNumMeshes; m_i++) {
+			const aiMesh* mesh = scene->mMeshes[m_i];
+			printf("    %i vertices in mesh\n", mesh->mNumVertices);
+			g_point_count[i] += mesh->mNumVertices;
+			for (unsigned int v_i = 0; v_i < mesh->mNumVertices; v_i++) {
+				if (mesh->HasPositions()) {
+					const aiVector3D* vp = &(mesh->mVertices[v_i]);
+					//printf ("      vp %i (%f,%f,%f)\n", v_i, vp->x, vp->y, vp->z);
+					g_vp[i].push_back(vp->x);
+					g_vp[i].push_back(vp->y);
+					g_vp[i].push_back(vp->z);
+				}
+				if (mesh->HasNormals()) {
+					const aiVector3D* vn = &(mesh->mNormals[v_i]);
+					//printf ("      vn %i (%f,%f,%f)\n", v_i, vn->x, vn->y, vn->z);
+					g_vn[i].push_back(vn->x);
+					g_vn[i].push_back(vn->y);
+					g_vn[i].push_back(vn->z);
+				}
+				if (mesh->HasTextureCoords(0)) {
+					const aiVector3D* vt = &(mesh->mTextureCoords[0][v_i]);
+					//printf ("      vt %i (%f,%f)\n", v_i, vt->x, vt->y);
+					g_vt[i].push_back(vt->x);
+					g_vt[i].push_back(vt->y);
+				}
+				if (mesh->HasTangentsAndBitangents()) {
+					// NB: could store/print tangents here
+				}
 			}
 		}
-	}
 
-	aiReleaseImport(scene);
+		aiReleaseImport(scene);
+	}
 	return true;
 }
 
@@ -351,103 +350,56 @@ GLuint CompileShaders()
 #pragma region VBO_FUNCTIONS
 
 void generateObjectBufferMesh() {
-/*----------------------------------------------------------------------------
-                   LOAD MESH HERE AND COPY INTO BUFFERS
-  ----------------------------------------------------------------------------*/
-
-	//Note: you may get an error "vector subscript out of range" if you are using this code for a mesh that doesnt have positions and normals
-	//Might be an idea to do a check for that before generating and binding the buffer.
-
-	load_mesh (MESH_NAME);
-
-	unsigned int vp_vbo = 0;
-
-	loc1 = glGetAttribLocation(shaderProgramID, "vertex_position");
-	loc2 = glGetAttribLocation(shaderProgramID, "vertex_normal");
-	loc3 = glGetAttribLocation(shaderProgramID, "vertex_texture");
-
-	glGenBuffers (1, &vp_vbo);
-	glBindBuffer (GL_ARRAY_BUFFER, vp_vbo);
-	glBufferData (GL_ARRAY_BUFFER, g_point_count * 3 * sizeof (float), &g_vp[0], GL_STATIC_DRAW);
-	unsigned int vn_vbo = 0;
-	glGenBuffers (1, &vn_vbo);
-	glBindBuffer (GL_ARRAY_BUFFER, vn_vbo);
-	glBufferData (GL_ARRAY_BUFFER, g_point_count * 3 * sizeof (float), &g_vn[0], GL_STATIC_DRAW);
-
-//	This is for texture coordinates which you don't currently need, so I have commented it out
-//	unsigned int vt_vbo = 0;
-//	glGenBuffers (1, &vt_vbo);
-//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
-//	glBufferData (GL_ARRAY_BUFFER, g_point_count * 2 * sizeof (float), &g_vt[0], GL_STATIC_DRAW);
-	
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray (vao);
-
-	glEnableVertexAttribArray (loc1);
-	glBindBuffer (GL_ARRAY_BUFFER, vp_vbo);
-	glVertexAttribPointer (loc1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-	glEnableVertexAttribArray (loc2);
-	glBindBuffer (GL_ARRAY_BUFFER, vn_vbo);
-	glVertexAttribPointer (loc2, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
-	glBindVertexArray (0);
-
-//	This is for texture coordinates which you don't currently need, so I have commented it out
-//	glEnableVertexAttribArray (loc3);
-//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
-//	glVertexAttribPointer (loc3, 2, GL_FLOAT, GL_FALSE, 0, NULL);
-	
-}
-
-void generateObjectBufferMesh2() {
 	/*----------------------------------------------------------------------------
 	LOAD MESH HERE AND COPY INTO BUFFERS
 	----------------------------------------------------------------------------*/
 
 	//Note: you may get an error "vector subscript out of range" if you are using this code for a mesh that doesnt have positions and normals
 	//Might be an idea to do a check for that before generating and binding the buffer.
+	const char* names[5];
+	names[0] = STADIUM_NAME;
+	names[1] = PLAYER_NAME;
+	names[2] = BALL_NAME;
+	names[3] = ENEMY_NAME;
+	names[4] = PLAYER_ARM;
+	load_mesh(names);
 
-	load_mesh2(MESH_NAME2);
+	for (int i = 0; i < numberOfMeshes; i++) {
+		unsigned int vp_vbo = 0;
 
-	unsigned int vp_vbo2 = 0;
+		loc1 = glGetAttribLocation(shaderProgramID, "vertex_position");
+		loc2 = glGetAttribLocation(shaderProgramID, "vertex_normal");
+		loc3 = glGetAttribLocation(shaderProgramID, "vertex_texture");
 
-	loc1 = glGetAttribLocation(shaderProgramID, "vertex_position");
-	loc2 = glGetAttribLocation(shaderProgramID, "vertex_normal");
-	loc3 = glGetAttribLocation(shaderProgramID, "vertex_texture");
+		glGenBuffers(1, &vp_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vp_vbo);
+		glBufferData(GL_ARRAY_BUFFER, g_point_count[i] * 3 * sizeof(float), &g_vp[i][0], GL_STATIC_DRAW);
+		unsigned int vn_vbo = 0;
+		glGenBuffers(1, &vn_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vn_vbo);
+		glBufferData(GL_ARRAY_BUFFER, g_point_count[i] * 3 * sizeof(float), &g_vn[i][0], GL_STATIC_DRAW);
 
-	//second mesh
-	glGenBuffers(1, &vp_vbo2);
-	glBindBuffer(GL_ARRAY_BUFFER, vp_vbo2);
-	glBufferData(GL_ARRAY_BUFFER, g_point_count2 * 3 * sizeof(float), &g_vp2[0], GL_STATIC_DRAW);
-	unsigned int vn_vbo2 = 0;
-	glGenBuffers(1, &vn_vbo2);
-	glBindBuffer(GL_ARRAY_BUFFER, vn_vbo2);
-	glBufferData(GL_ARRAY_BUFFER, g_point_count2 * 3 * sizeof(float), &g_vn2[0], GL_STATIC_DRAW);
+		//	This is for texture coordinates which you don't currently need, so I have commented it out
+		//	unsigned int vt_vbo = 0;
+		//	glGenBuffers (1, &vt_vbo);
+		//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
+		//	glBufferData (GL_ARRAY_BUFFER, g_point_count * 2 * sizeof (float), &g_vt[0], GL_STATIC_DRAW);
 
-	//	This is for texture coordinates which you don't currently need, so I have commented it out
-	//	unsigned int vt_vbo = 0;
-	//	glGenBuffers (1, &vt_vbo);
-	//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
-	//	glBufferData (GL_ARRAY_BUFFER, g_point_count * 2 * sizeof (float), &g_vt[0], GL_STATIC_DRAW);
+		glGenVertexArrays(1, &vaos[i]);
+		glBindVertexArray(vaos[i]);
 
-	glGenVertexArrays(1, &vao2);
-	glBindVertexArray(vao2);
-	glEnableVertexAttribArray(loc1);
-	glBindBuffer(GL_ARRAY_BUFFER, vp_vbo2);
-	glVertexAttribPointer(loc1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-	glEnableVertexAttribArray(loc2);
-	glBindBuffer(GL_ARRAY_BUFFER, vn_vbo2);
-	glVertexAttribPointer(loc2, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		glEnableVertexAttribArray(loc1);
+		glBindBuffer(GL_ARRAY_BUFFER, vp_vbo);
+		glVertexAttribPointer(loc1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		glEnableVertexAttribArray(loc2);
+		glBindBuffer(GL_ARRAY_BUFFER, vn_vbo);
+		glVertexAttribPointer(loc2, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-	glBindVertexArray(0);
-
-
-	//	This is for texture coordinates which you don't currently need, so I have commented it out
-	//	glEnableVertexAttribArray (loc3);
-	//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
-	//	glVertexAttribPointer (loc3, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+		glBindVertexArray(0);
+	}
 
 }
+
 
 
 #pragma endregion VBO_FUNCTIONS
@@ -475,7 +427,7 @@ void display(){
 	if (throwBall = throwBall) {
 		cameraPos = glm::vec3(xBall, yBall, (zBall+1));
 	}
-	// Root of the Hierarchy
+	// Root of the Hierarchy (stadium)
 	glm::mat4 view = glm::translate(glm::mat4(1.0), glm::vec3(0.0, 0.0, -40.0));
 	mat4 persp_proj = perspective(45.0, (float)width/(float)height, 0.1, 100.0);
 	mat4 model = identity_mat4 ();
@@ -487,17 +439,17 @@ void display(){
 	glUniformMatrix4fv (view_mat_location, 1, GL_FALSE, glm::value_ptr(view));
 	glUniformMatrix4fv (matrix_location, 1, GL_FALSE, model.m);
 
-	// draw mesh 1 (stadium)
-	glBindVertexArray(vao);
-	glDrawArrays (GL_TRIANGLES, 0, g_point_count);
+	// draw mesh 1 (player)
+	glBindVertexArray(vaos[0]);
+	glDrawArrays (GL_TRIANGLES, 0, g_point_count[0]);
 
 	mat4 model2 = identity_mat4();
 	model2 = translate(model2, vec3(xplayer, -0.09f, zplayer));
 	mat4 globalmodel = model * model2;
 	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, globalmodel.m);
 
-	glBindVertexArray(vao2);
-	glDrawArrays(GL_TRIANGLES, 0, g_point_count2);
+	glBindVertexArray(vaos[1]);
+	glDrawArrays(GL_TRIANGLES, 0, g_point_count[1]);
 
 	// draw mesh 2 (ball)
 	if (throwBall) {
@@ -506,23 +458,45 @@ void display(){
 		mat4 globalmodel2 = globalmodel * model3;
 		glUniformMatrix4fv(matrix_location, 1, GL_FALSE, globalmodel2.m);
 
-		glDrawArrays(GL_TRIANGLES, 0, g_point_count2);
+		glBindVertexArray(vaos[2]);
+		glDrawArrays(GL_TRIANGLES, 0, g_point_count[2]);
 		//throwBall = false;
 	}
 
 	// draw enemy keep array of enemies (can increase it depending on which level)
-	printf("z position::::  %f", enemies[0].getZPos());
+	//printf("z position::::  %f", enemies[0].getZPos());
 
 	mat4 model4 = identity_mat4();
 	model4 = translate(model4, vec3(enemies[0].getXPos(), enemies[0].getYPos(), enemies[0].getZPos()));
 	mat4 globalmodel3 = globalmodel * model4;
 	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, globalmodel3.m);
-	glDrawArrays(GL_TRIANGLES, 0, g_point_count2);
+
+	glBindVertexArray(vaos[3]);
+	glDrawArrays(GL_TRIANGLES, 0, g_point_count[3]);
 
 
+	//throwing animation
+	if (throwing) {
+		mat4 model4 = identity_mat4();
+		model4 = rotate_x_deg(model4, 180.0f);
+		model4 = translate(model4, vec3(0.095f, 0.44f, 0.0f));
+		mat4 globalmodel3 = globalmodel * model4;
+		glUniformMatrix4fv(matrix_location, 1, GL_FALSE, globalmodel3.m);
 
+		glBindVertexArray(vaos[4]);
+		glDrawArrays(GL_TRIANGLES, 0, g_point_count[4]);
+	}
+	else {
+		mat4 model4 = identity_mat4();
+		//model4 = rotate_x_deg(model4, 180.0f);
+		model4 = translate(model4, vec3(0.095f, 0.44f, 0.0f));
+		mat4 globalmodel3 = globalmodel * model4;
+		glUniformMatrix4fv(matrix_location, 1, GL_FALSE, globalmodel3.m);
 
-
+		glBindVertexArray(vaos[4]);
+		glDrawArrays(GL_TRIANGLES, 0, g_point_count[4]);
+	}
+	//make timer, end of timer release ball, during timer move arm up then back down using throwing boolean function
 
 
     glutSwapBuffers();
@@ -548,7 +522,7 @@ void updateScene() {
 	zBall -= 0.1f;
 
 	//move enemy towards player
-	enemies[0].moveToPlayer();
+	//enemies[0].moveToPlayer();
 
 	// Draw the next frame
 	glutPostRedisplay();
@@ -569,7 +543,6 @@ void init()
 	GLuint shaderProgramID = CompileShaders();
 	// load mesh into a vertex buffer array
 	generateObjectBufferMesh();
-	generateObjectBufferMesh2();
 	
 }
 
@@ -617,6 +590,14 @@ void keypress(unsigned char key, int x, int y) {
 		yBall = 0.0f;
 		zBall = 0.0f;
 		throwBall = true;
+	}
+
+	//move arm
+	if (key == 'o') {
+		throwing = true;
+	}
+	if (key == 'p') {
+		throwing = false;
 	}
 }
 
